2a28d69f316ecc6f5dcebd4882c3385cc97a1408
kuailexs <952415538@qq.com>
2023-11-18 17:24:53 +0800

    一点点

--- src/vendor_qti_ims_call.c
+++ src/vendor_qti_ims_call.c
@@ -1,5 +1,25 @@
 
 
+
+typedef BinderExtCallClass VendorQtiImsCallClass;
+typedef struct QtiImsCall {
+    BinderExtCall parent;
+    void * v_1;
+    void * v_2;
+    void * v_3;
+    void * v_4;
+    void * v_5;
+    void * v_6;
+} VendorQtiImsCall;
+
+G_DEFINE_TYPE(VendorQtiImsCall, qti_ims_call, BINDER_EXT_TYPE_CALL)
+
+#define QTI_IMS_CALL_TYPE_SLOT qti_ims_call_get_type()
+#define QTI_IMS_CALL_SLOT(obj) G_TYPE_CHECK_INSTANCE_CAST(obj, QTI_IMS_CALL_TYPE_SLOT, VendorQtiImsCall)
+#define QTI_IMS_CALL_IS_SLOT(obj) G_TYPE_CHECK_INSTANCE_TYPE(obj, QTI_IMS_CALL_TYPE_SLOT)
+
+
+
 vendor_qti_ims_call_conference_info_start_element
 vendor_qti_ims_call_reg_state_update
 vendor_qti_ims_call_hangup
